b76c3ca59ff3ed1ed6fd942843476b329ef6e32b
Junior Almeida <jrjunioralmeida@outlook.com>
2023-10-10 11:53:34 -0300

    Correções

--- script.js
+++ script.js
@@ -7,11 +7,11 @@ function toggleMode() {
     const alt = document.querySelector("#profile img")
 
     if (html.classList.contains('light')) {
-        img.setAttribute('src', './assets/assets/avatar-light.png')
+        img.setAttribute('src', './assets/avatar-light.png')
         alt.setAttribute('alt', 'Foto de Mayk Brito sorrindo, usando óculos, camisa preta e casaco, sem barba e fundo degrade de roxo e azul.')
     }
     else{
-       img.setAttribute('src', './assets/assets/avatar.png') 
+       img.setAttribute('src', './assets/avatar.png') 
        alt.setAttribute('alt', 'Foto de Mayk Brito sorrindo, usando óculos e camisa preta, barba e fundo amarelo.')
     }
 }
